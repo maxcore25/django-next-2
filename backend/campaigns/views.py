95dca2e6f07a39e5e08ee5b3f5db0116f72398ce
maxcore25 <mkorobov2002@mail.ru>
2022-03-02 06:46:20 +0300

    Get Campaign Endpoint Written

--- backend/campaigns/views.py
+++ backend/campaigns/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Campaign
 
-# Create your views here.
+
+class CampaignListAPIView(generics.ListAPIView):
+    def get_queryset(self):
+        return Campaign.objects.all()
