f924fe5bd37e21503430ceac7bf4274d37003cb0
maxcore25 <mkorobov2002@mail.ru>
2022-03-02 06:53:12 +0300

    Serializers Written

--- backend/campaigns/views.py
+++ backend/campaigns/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Campaign
+from .serializers import CampaignSerializer, SubscriberSerializer
 
 
 class CampaignListAPIView(generics.ListAPIView):
+    serializer_class = CampaignSerializer
+
     def get_queryset(self):
         return Campaign.objects.all()
