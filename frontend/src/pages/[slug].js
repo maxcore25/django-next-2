f074682eda3eb94229a98fd42cb2fc2f8d36ce92
maxcore25 <mkorobov2002@mail.ru>
2022-03-02 13:19:48 +0300

    Subscribe Form Added

--- frontend/src/pages/[slug].js
+++ frontend/src/pages/[slug].js
@@ -1,10 +1,29 @@
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import dateFormat from 'dateformat';
+import { useState } from 'react';
 
 const Campaign = ({ data }) => {
   console.log(data);
   const route = useRouter();
+
+  const [email, setEmail] = useState('');
+
+  const handleOnSubmit = e => {
+    e.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, campaign: data.id }),
+    };
+    fetch(`${process.env.BASE_URL}subscribers`, options)
+      .then(res => res.json())
+      .then(response => console.log('response', response))
+      .catch(error => console.log('error', error));
+  };
 
   return (
     <div>
@@ -39,6 +58,20 @@ const Campaign = ({ data }) => {
           </small>
         </div>
       </div>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          <input
+            onChange={event => setEmail(event.target.value)}
+            type='email'
+            name='email'
+            placeholder='Enter email'
+            required
+          />
+        </div>
+        <div>
+          <input type='submit' value='SUBSCRIBE' />
+        </div>
+      </form>
     </div>
   );
 };
