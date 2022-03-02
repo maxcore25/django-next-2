465f260b4d7cc11538c18005cefe5daf42668a12
maxcore25 <mkorobov2002@mail.ru>
2022-03-02 12:38:29 +0300

    Static Generation Functions Written

--- frontend/src/pages/[slug].js
+++ frontend/src/pages/[slug].js
@@ -1,7 +1,9 @@
 import { useRouter } from 'next/router';
 
-const Campaign = () => {
+const Campaign = ({ data }) => {
+  console.log(data);
   const route = useRouter();
+
   return (
     <div>
       <h1>{route.query.slug}</h1>
@@ -10,13 +12,28 @@ const Campaign = () => {
 };
 
 export async function getStaticPaths() {
-  const response = await fetch(`${process.env.BASE_URL}/campaigns`);
-  data = await response.json();
+  const response = await fetch(`${process.env.BASE_URL}campaigns`);
+  const data = await response.json();
+  const allSlugs = data.map(item => item.slug);
+  console.log('slugs:', allSlugs);
+  const paths = allSlugs.map(slug => ({ params: { slug: slug } }));
+  console.log('paths:', paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const response = await fetch(
+    `${process.env.BASE_URL}campaigns/${params.slug}`
+  );
+  const data = await response.json();
 
   return {
     props: {
       data,
-      error,
     },
   };
 }
