9c589c0b1cc37ebfdd0ffef86fd2dab142968dcb
maxcore25 <mkorobov2002@mail.ru>
2022-03-02 12:58:44 +0300

    Some Components On The Dynamic Page Added

--- frontend/src/pages/[slug].js
+++ frontend/src/pages/[slug].js
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import dateFormat from 'dateformat';
 
 const Campaign = ({ data }) => {
   console.log(data);
@@ -7,6 +9,36 @@ const Campaign = ({ data }) => {
   return (
     <div>
       <h1>{route.query.slug}</h1>
+      <div
+        style={{
+          border: '1px solid #000',
+          display: 'flex',
+          gap: 20,
+          alignItems: 'center',
+          padding: 20,
+          margin: 10,
+        }}>
+        <div>
+          <Image
+            src={`https://res.cloudinary.com/dkd3mgbyi/${data.logo}`}
+            alt='hello'
+            width={120}
+            height={120}
+          />
+        </div>
+        <div>
+          <h1>
+            {data.id} - {data.title}
+          </h1>
+          <p>{data.description}</p>
+          <small>
+            {dateFormat(
+              new Date(data.created_at),
+              'ddd, mmmm, dS, yyyy, h:MM:ss TT'
+            )}
+          </small>
+        </div>
+      </div>
     </div>
   );
 };
