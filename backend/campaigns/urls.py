2b659f121999878f52949e0e855d3271aabc5480
maxcore25 <mkorobov2002@mail.ru>
2022-03-02 07:34:39 +0300

    Campaign Detail Endpoint Written

--- backend/campaigns/urls.py
+++ backend/campaigns/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CampaignListAPIView
+from .views import CampaignListAPIView, CampaignDetailAPIView
 
 urlpatterns = [
-    path('campaigns/', CampaignListAPIView.as_view(), name='campaigns')
+    path('campaigns/', CampaignListAPIView.as_view(), name='campaigns'),
+    path('campaigns/<str:slug>', CampaignDetailAPIView.as_view(), name='campaign'),
 ]
--- backend/campaigns/views.py
+++ backend/campaigns/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, response, status
 from .models import Campaign
 from .serializers import CampaignSerializer, SubscriberSerializer
 
@@ -9,3 +9,13 @@ class CampaignListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         return Campaign.objects.all()
+
+
+class CampaignDetailAPIView(generics.GenericAPIView):
+    serializer_class = CampaignSerializer
+
+    def get(self, request, slug):
+        query_set = Campaign.objects.filter(slug=slug).first()
+        if query_set:
+            return response.Response(self.serializer_class(query_set).data)
+        return response.Response('Not Found', status=status.HTTP_404_NOT_FOUND)
